3779b55c74a60a58896fb1c71d1e8e564c1b0980
cowtowncoder <cowtowncoder@a471a628-dde7-0310-ad75-8f36f6559047>
2005-06-04 06:17:14 +0000

    Working more on writer side of StaxMate.

--- staxmate/src/java/org/codehaus/staxmate/sw/SMBufferedFragment.java
+++ staxmate/src/java/org/codehaus/staxmate/sw/SMBufferedFragment.java
@@ -12,94 +12,115 @@ public final class SMBufferedFragment
     extends SMOutputContainer
     implements SMBufferable
 {
+    /* These are the distinct states:
+     *
+     * BUFFERED_AND_BLOCKED: initial state, where output is blocked both
+     *   by this fragment being buffered, AND parent being blocked by
+     *   some other buffering.
+     * BUFFERED: initial state, where output is only blocked due to the
+     *   fragment itself being buffered.
+     * BLOCKED: optional state; will be moved here if fragment is unbuffered
+     *   but parent has not yet indicate it is ready to be unblocked
+     * OPEN: state in which we can freely output children
+     * CLOSED: state during which no children can be added any more
+     */
+    protected final static int STATE_BUFFERED_AND_BLOCKED = 1;
+    protected final static int STATE_BUFFERED = 2;
+    protected final static int STATE_BLOCKED = 3;
+    protected final static int STATE_OPEN = 4;
+    protected final static int STATE_CLOSED = 5;
+
+    protected final static int LAST_BUFFERED = STATE_BUFFERED;
+    protected final static int LAST_BLOCKED = STATE_BLOCKED;
+
     /**
      * All instances are initially buffered; state will be changed when
      * instance is released.
      */
-    protected boolean mIsBuffered = true;
+    protected int mState = STATE_BUFFERED;
 
     protected SMBufferedFragment(SMOutputContext ctxt)
     {
-	super(ctxt, null);
+        super(ctxt, null);
     }
 
     /*
     ///////////////////////////////////////////////////////////
     // SMBufferable implementation
     ///////////////////////////////////////////////////////////
-     */
+    */
 
     public boolean isBuffered() {
-	return mIsBuffered;
+        return (mState <= LAST_BUFFERED);
     }
 
     public void linkParent(SMOutputContainer parent) {
-	if (mParent != null) {
-	    throw new IllegalStateException("Can not re-set parent once it has been set once");
-	}
-	mParent = parent;
+        if (mParent != null) {
+            throw new IllegalStateException("Can not re-set parent once it has been set once");
+        }
+        mParent = parent;
     }
 
     public void release()
-	throws XMLStreamException
+        throws XMLStreamException
     {
-	mIsBuffered = false;
-	if (mParent != null) {
-	    mParent.childReleased(this);
-	}
+        //mIsBuffered = false;
+        if (mParent != null) {
+            mParent.childReleased(this);
+        }
     }
 
     /*
     ///////////////////////////////////////////////////////////
     // Abstract method implementations
     ///////////////////////////////////////////////////////////
-     */
+    */
 
     protected void childReleased(SMLinkedOutput child)
-	throws XMLStreamException
+        throws XMLStreamException
     {
-	// First, if we are buffered, no need to do anything more...
-	if (mIsBuffered) {
-	    return;
-	}
-	/* Otherwise, the only significant child is the first one, as it's
-	 * the only one that may have blocked output:
-	 */
-	if (child == mFirstChild) {
-	    // If so, parent can (and should) deal with it... if we have one
-	    if (mParent != null) {
-		mParent.childReleased(this);
-	    }
-	}
+        // First, if we are buffered, no need to do anything more...
+        if (mState <= LAST_BLOCKED) {
+            return;
+        }
+        /* Otherwise, the only significant child is the first one, as it's
+         * the only one that may have blocked output:
+         */
+        if (child == mFirstChild) {
+            // If so, parent can (and should) deal with it... if we have one
+            if (mParent != null) {
+                mParent.childReleased(this);
+            }
+        }
     }
 
     protected boolean doOutput(boolean canClose)
-	throws XMLStreamException
+        throws XMLStreamException
     {
-	// No outputting if still buffered...
-	if (mIsBuffered) {
-	    return false;
-	}
-	if (canClose) {
-	    return closeAndOutputChildren();
-	}
-	return closeAllButLastChild();
+        // No outputting if still buffered...
+        if (mState <= LAST_BLOCKED) {
+            return false;
+        }
+        if (canClose) {
+            return closeAndOutputChildren();
+        }
+        return closeAllButLastChild();
     }
 
     protected void forceOutput()
-	throws XMLStreamException
+        throws XMLStreamException
     {
-	mIsBuffered = false;
-	forceChildOutput();
+        //mIsBuffered = false;
+        forceChildOutput();
     }
 
     public boolean canOutputNewChild()
-	throws XMLStreamException
+        throws XMLStreamException
     {
-	// Can not just output if we are buffered...
-	if (mIsBuffered) {
-	    return false;
-	}
-	return closeAndOutputChildren();
+        // Can not just output if we are buffered...
+        if (mState <= LAST_BLOCKED) {
+            return false;
+        }
+        return closeAndOutputChildren();
     }
 }
