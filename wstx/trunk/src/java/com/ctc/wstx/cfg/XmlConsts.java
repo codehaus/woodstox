1cb009b3a0cc7275b190ebdacffac60c00b384c1
cowtowncoder <cowtowncoder@a471a628-dde7-0310-ad75-8f36f6559047>
2006-02-07 05:00:10 +0000

    Forgot to check this in earlier, doh.

---
+++ wstx/trunk/src/java/com/ctc/wstx/cfg/XmlConsts.java
@@ -0,0 +1,20 @@
+package com.ctc.wstx.cfg;
+
+/**
+ * Simple constant container interface, shared by input and output
+ * sides.
+ */
+public interface XmlConsts
+{
+    // // // Constants for XML declaration
+
+    public final static String XML_DECL_KW_ENCODING = "encoding";
+    public final static String XML_DECL_KW_VERSION = "version";
+    public final static String XML_DECL_KW_STANDALONE = "standalone";
+
+    public final static String XML_V_10 = "1.0";
+    public final static String XML_V_11 = "1.1";
+
+    public final static String XML_SA_YES = "yes";
+    public final static String XML_SA_NO = "no";
+}
