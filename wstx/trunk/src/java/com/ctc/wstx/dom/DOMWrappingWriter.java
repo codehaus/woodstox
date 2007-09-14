ea961b8170f553a42b424549a208a7ef4d90b0a6
cowtowncoder <cowtowncoder@a471a628-dde7-0310-ad75-8f36f6559047>
2007-09-14 05:31:31 +0000

    Starting to work on finishing DOM-backed stream writer implementation.

--- wstx/trunk/src/java/com/ctc/wstx/dom/DOMWrappingWriter.java
+++ wstx/trunk/src/java/com/ctc/wstx/dom/DOMWrappingWriter.java
@@ -20,8 +20,7 @@ import com.ctc.wstx.api.WstxOutputProperties;
 
 /**
  * This is an adapter class that allows building a DOM tree using
- * {@link XMLStreamWriter} interface. It implements a sufficiently
- * complement subset of functionality, so 
+ * {@link XMLStreamWriter} interface.
  *<p>
  * Note that the implementation is only to be used for use with
  * <code>javax.xml.transform.dom.DOMResult</code>.
@@ -29,6 +28,9 @@ import com.ctc.wstx.api.WstxOutputProperties;
  * Some notes regarding missing/incomplete functionality:
  * <ul>
  *  </ul>
+ *
+ * @author Tatu Saloranta
+ * @author Dan Diephouse
  */
 public class DOMWrappingWriter
     implements XMLStreamWriter2
@@ -50,11 +52,23 @@ public class DOMWrappingWriter
      */
     protected String mEncoding = null;
 
+    /**
+     * If we are being given info about existing bindings, it'll come
+     * as a NamespaceContet.
+     */
+    protected NamespaceContext mNsContext;
+
     /*
     ////////////////////////////////////////////////////
     // State
     ////////////////////////////////////////////////////
      */
+
+    /**
+     * We need a reference to the document hosting nodes to
+     * be able to create new nodes
+     */
+    protected final Document mDocument;
 
     protected Node mCurrNode;
 
@@ -79,13 +93,19 @@ public class DOMWrappingWriter
          */
         switch (treeRoot.getNodeType()) {
         case Node.DOCUMENT_NODE: // fine
+            mDocument = (Document) treeRoot;
+
             /* Should try to find encoding, version and stand-alone
              * settings... but is there a standard way of doing that?
              */
+            break;
+
         case Node.ELEMENT_NODE: // can make sub-tree... ok
-            // But should we skip START/END_DOCUMENT? For now, let's not
+            mDocument = treeRoot.getOwnerDocument();
+            break;
 
         case Node.DOCUMENT_FRAGMENT_NODE: // as with element...
+            mDocument = treeRoot.getOwnerDocument();
 
             // Above types are fine
             break;
@@ -94,6 +114,10 @@ public class DOMWrappingWriter
             throw new XMLStreamException("Can not create an XMLStreamWriter for a DOM node of type "+treeRoot.getClass());
         }
         mCurrNode = treeRoot;
+
+        if (mDocument == null) {
+            throw new XMLStreamException("Can not create an XMLStreamWriter for given node (of type "+treeRoot.getClass()+"): did not have owner document");
+        }
     }
 
     public static DOMWrappingWriter createFrom(WriterConfig cfg, DOMResult dst)
@@ -109,11 +133,13 @@ public class DOMWrappingWriter
     ////////////////////////////////////////////////////
      */
 
-    public void close()
-    {
+    public void close() {
+        // NOP
     }
     
-    public void flush() {}
+    public void flush() {
+        // NOP
+    }
 
     public NamespaceContext getNamespaceContext() {
         // !!! TBI
@@ -121,37 +147,126 @@ public class DOMWrappingWriter
     }
 
     public String getPrefix(String uri) {
+        // !!! TBI
         return null;
     }
 
     public Object getProperty(String name) {
+        // !!! TBI
         return null;
     }
 
-    public void setDefaultNamespace(String uri) {}
-    public void setNamespaceContext(NamespaceContext context) {}
-    public void setPrefix(String prefix, String uri) {}
-    public void writeAttribute(String localName, String value) {}
-    public void writeAttribute(String namespaceURI, String localName, String value) {}
-    public void writeAttribute(String prefix, String namespaceURI, String localName, String value) {}
-    public void writeCData(String data) {}
-    public void writeCharacters(char[] text, int start, int len) {}
-    public void writeCharacters(String text) {}
-    public void writeComment(String data) {}
-    public void writeDefaultNamespace(String namespaceURI) {}
-    public void writeDTD(String dtd) {}
-    public void writeEmptyElement(String localName) {}
-    public void writeEmptyElement(String namespaceURI, String localName) {}
-    public void writeEmptyElement(String prefix, String localName, String namespaceURI) {}
-    public void writeEndDocument() {}
-    public void writeEndElement() {}
-    public void writeEntityRef(String name) {}
-    public void writeNamespace(String prefix, String namespaceURI) {}
-    public void writeProcessingInstruction(String target) {}
-    public void writeProcessingInstruction(String target, String data) {}
+    public void setDefaultNamespace(String uri) {
+        // !!! TBI
+    }
 
-    public void writeSpace(char[] text, int start, int len) {}
-    public void writeSpace(String text) {}
+    public void setNamespaceContext(NamespaceContext context) {
+        mNsContext = context;
+    }
+
+    public void setPrefix(String prefix, String uri) {
+        // !!! TBI
+    }
+
+    public void writeAttribute(String localName, String value)
+    {
+        writeAttribute(null, localName, value);
+    }
+
+    public void writeAttribute(String namespaceURI, String localName, String value) {
+        checkIsElement();
+        // !!! TBI
+    }
+
+    public void writeAttribute(String prefix, String namespaceURI, String localName, String value)
+    {
+        checkIsElement();
+        // !!! TBI
+    }
+
+    public void writeCData(String data) {
+        mCurrNode.appendChild(mDocument.createCDATASection(data));
+    }
+
+    public void writeCharacters(char[] text, int start, int len)
+    {
+        writeCharacters(new String(text, start, len));
+    }
+    
+    public void writeCharacters(String text) {
+        mCurrNode.appendChild(mDocument.createTextNode(text));
+    }
+
+    public void writeComment(String data) {
+        mCurrNode.appendChild(mDocument.createCDATASection(data));
+    }
+
+    public void writeDefaultNamespace(String namespaceURI) {
+        checkIsElement();
+
+        // !!! TBI
+    }
+
+    public void writeDTD(String dtd)
+    {
+        // Would have to parse, not worth trying...
+        reportUnsupported("writeDTD()");
+    }
+
+    public void writeEmptyElement(String localName) {
+        writeEmptyElement(null, localName);
+    }
+
+    public void writeEmptyElement(String namespaceURI, String localName)
+    {
+        // Note: can not just call writeStartElement(), since this should not change context for good!
+
+        // !!! TBI
+    }
+
+    public void writeEmptyElement(String prefix, String localName, String namespaceURI)
+    {
+        // !!! TBI
+    }
+
+    public void writeEndDocument()
+    {
+        mCurrNode = mDocument;
+    }
+
+    public void writeEndElement() {
+    }
+
+    public void writeEntityRef(String name) {
+        mCurrNode.appendChild(mDocument.createEntityReference(name));
+    }
+
+    public void writeNamespace(String prefix, String namespaceURI) {
+        checkIsElement();
+
+        // !!! TBI
+    }
+
+    public void writeProcessingInstruction(String target) {
+        writeProcessingInstruction(target, null);
+    }
+
+    public void writeProcessingInstruction(String target, String data) {
+        mCurrNode.appendChild(mDocument.createProcessingInstruction(target, data));
+    }
+
+    public void writeSpace(char[] text, int start, int len) {
+        writeSpace(new String(text, start, len));
+    }
+
+    public void writeSpace(String text) {
+        /* This won't work all that well, given there's no way to
+         * prevent quoting/escaping. But let's do what we can, since
+         * the alternative (throwing an exception) doesn't seem
+         * especially tempting choice.
+         */
+        writeCharacters(text);
+    }
 
     public void writeStartDocument()
     {
@@ -159,16 +274,27 @@ public class DOMWrappingWriter
                            WstxOutputProperties.DEFAULT_XML_VERSION);
     }
 
-    public void writeStartDocument(String version) {}
+    public void writeStartDocument(String version)
+    {
+        writeStartDocument(null, version);
+    }
 
     public void writeStartDocument(String encoding, String version)
     {
+        // Is there anything here we can or should do? No?
         mEncoding = encoding;
     }
 
-    public void writeStartElement(String localName) {}
-    public void writeStartElement(String namespaceURI, String localName) {}
+    public void writeStartElement(String localName) {
+        writeStartElement(null, localName);
+    }
+
+    public void writeStartElement(String namespaceURI, String localName) {
+        // !!! TBI
+    }
+
     public void writeStartElement(String prefix, String localName, String namespaceURI) {
+        // !!! TBI
     } 
 
     /*
@@ -229,26 +355,33 @@ public class DOMWrappingWriter
     public void writeCData(char[] text, int start, int len)
         throws XMLStreamException
     {
-        // !!! TBI
+        writeCData(new String(text, start, len));
     }
 
     public void writeDTD(String rootName, String systemId, String publicId,
                          String internalSubset)
         throws XMLStreamException
     {
-        // !!! TBI
+        /* Alas: although we can create a DocumentType object, there
+         * doesn't seem to be a way to attach it in DOM-2!
+         */
+        if (mCurrNode != mDocument) {
+            throw new IllegalStateException("Operation only allowed to the document before adding root element");
+        }
+        reportUnsupported("writeDTD()");
     }
 
     public void writeFullEndElement() throws XMLStreamException
     {
-        // !!! TBI
+        // No difference with DOM
+        writeEndElement();
     }
 
     public void writeStartDocument(String version, String encoding,
                                    boolean standAlone)
         throws XMLStreamException
     {
-        // !!! TBI
+        writeStartDocument(encoding, version);
     }
     
     /*
@@ -260,24 +393,43 @@ public class DOMWrappingWriter
     public void writeRaw(String text)
         throws XMLStreamException
     {
-        // !!! TBI: Can it be implemented at all?
+        reportUnsupported("writeRaw()");
     }
 
     public void writeRaw(String text, int start, int offset)
         throws XMLStreamException
     {
-        // !!! TBI: Can it be implemented at all?
+        reportUnsupported("writeRaw()");
     }
 
     public void writeRaw(char[] text, int offset, int length)
         throws XMLStreamException
     {
-        // !!! TBI
+        reportUnsupported("writeRaw()");
     }
 
     public void copyEventFromReader(XMLStreamReader2 r, boolean preserveEventData)
         throws XMLStreamException
     {
         // !!! TBI
+    }
+
+    /*
+    ///////////////////////////////
+    // Stax2, pass-through methods
+    ///////////////////////////////
+    */
+
+    private void reportUnsupported(String operName)
+    {
+        throw new UnsupportedOperationException(operName+" can not be used with DOM-backed writer");
+    }
+
+    private void checkIsElement()
+    {
+        int type = mCurrNode.getNodeType();
+        if (type != Node.ELEMENT_NODE) {
+            throw new IllegalStateException("Operation only allowed when last output event was START_ELEMENT (context node is of type "+type+")");
+        }
     }
 }
