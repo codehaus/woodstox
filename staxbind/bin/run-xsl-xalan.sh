0933aada9502246c60582b6656bd7742307b2a88
cowtowncoder <cowtowncoder@a471a628-dde7-0310-ad75-8f36f6559047>
2009-02-09 20:18:59 +0000

    Added separate run commands for saxon, xalan tests.

---
+++ staxbind/bin/run-xsl-xalan.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+ 
+echo "About to run full xsl test on parser+processor combos (input dir: $1)"
+
+# Nothing big stored in memory, heap can remain modest 
+# -Djapex.runTime=30 \
+$JAVA_HOME/bin/java -server -cp lib/\* \
+ -Xmx128M \
+ -Djava.awt.headless=true \
+ -Djapex.runsPerDriver=1 \
+ -Djapex.warmupTime=5 \
+ -Djapex.runTime=30 \
+ -Djapex.numberOfThreads=1 \
+ -Djapex.reportsDirectory=japex-reports \
+ -Djapex.plotGroupSize=10 \
+ -Djapex.inputDir="$1" \
+ com.sun.japex.Japex \
+ testcfg/xslt-xalan.xml
+
+echo "Done!";
