5308c0bcbe6b44b9c81e5b04d8e0de8a86e8473f
cowtowncoder <cowtowncoder@a471a628-dde7-0310-ad75-8f36f6559047>
2009-02-09 23:55:51 +0000

    One more driver, for "string tree" parser. Another one bites the dust...

---
+++ staxbind/src/java/org/codehaus/staxbind/jsoncount/StringTreeDriver.java
@@ -0,0 +1,56 @@
+package org.codehaus.staxbind.jsoncount;
+
+import java.io.*;
+import java.util.*;
+
+import org.stringtree.json.JSONReader;
+
+/**
+ * Test driver that uses the "StringTree" json parser, from
+ * [http://stringtree.org/stringtree-json.html]
+ */
+public final class StringTreeDriver
+    extends JsonCountDriver
+{
+    public JsonSimpleDriver() {
+        _parser = new JSONParser();
+    }
+
+    protected void read(byte[] docData, CountResult results)
+        throws Exception
+    {
+        // Ugh. Yet another toy parser that requires a String...
+        String doc = new String(docData, "UTF-8");        
+        Object ob = new JSONReader(doc);
+        // but at least we get Maps, Lists etc
+        _processNode(ob, results);
+    }
+
+    private void _processNode(Object node, CountResult results)
+        throws Exception
+    {
+        if (node instanceof Map) {
+            _processObject((Map<?,?>) node, results);
+        } else if (node instanceof List) {
+            _processArray((List) node, results);
+        }
+    }
+
+    private void _processArray(List array, CountResult results)
+        throws Exception
+    {
+        for (Object ob : array) {
+            _processNode(ob, results);
+        }
+    }
+
+    private void _processObject(Map<?,?> object, CountResult results)
+        throws Exception
+    {
+        for (Map.Entry<?,?> en : object.entrySet()) {
+            String fn = (String) en.getKey();
+            results.addReference(fn);
+            _processNode(en.getValue(), results);
+        }
+    }
+}
