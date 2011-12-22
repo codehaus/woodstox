4a4aaf7d0599779b45a61a5e2edfcdfbccbb163d
cowtowncoder <cowtowncoder@a471a628-dde7-0310-ad75-8f36f6559047>
2011-12-22 21:07:56 +0000

    One more unit test

--- wstx/branches/4.1/src/test/stax2/stream/TestStreamReader.java
+++ wstx/branches/4.1/src/test/stax2/stream/TestStreamReader.java
@@ -1,5 +1,7 @@
 package stax2.stream;
 
+import java.io.ByteArrayInputStream;
+
 import javax.xml.stream.*;
 
 import org.codehaus.stax2.*;
@@ -27,5 +29,31 @@ public class TestStreamReader
         assertEquals(CHARACTERS, sr.getEventType());
         assertTrue(sr.isCharacters());
         assertTokenType(END_ELEMENT, sr.next());
+    }
+
+    /**
+     * Unit test related to [WSTX-274]
+     */
+    public void testCData() throws Exception
+    {
+        XMLInputFactory2 f = getInputFactory();
+        setNamespaceAware(f, true);
+        setCoalescing(f, true);
+        String strMessage = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+                "<soapenv:Body>" +
+                  "<echoRequest xmlns=\"http://test.ibm.com/xsd\">" +
+                    "<arg0>outside cdata <![CDATA[<data>inside cdata</data>]]></arg0>" +
+                  "</echoRequest>" +
+                "</soapenv:Body>" +
+              "</soapenv:Envelope>";                  
+          XMLStreamReader reader = f.createXMLStreamReader(new ByteArrayInputStream(strMessage.getBytes("UTF-8")));
+          assertTokenType(START_ELEMENT, reader.next());
+          assertTokenType(START_ELEMENT, reader.next());
+          assertTokenType(START_ELEMENT, reader.next());
+          assertTokenType(START_ELEMENT, reader.next());
+          // since we are coalescing, must be reported as CHARACTERS, not CDATA
+          assertTokenType(CHARACTERS, reader.next());
+          String cdata = new String(reader.getTextCharacters(), reader.getTextStart(), reader.getTextLength());
+          assertEquals("outside cdata <data>inside cdata</data>", cdata);
     }
 }
